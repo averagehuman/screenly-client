d84371302fd787c8ce1dcee020f334ef7751543b
averagehuman <cyclebelfast@gmail.com>
2018-02-20 06:28:29 +0000

    Added AssetSummary type to avoid unmarshalling problems.
    The screenly api is not strict in terms of the types of the JSON fields
    that it accepts and returns, eg. Duration can be given as a string or an
    integer. In particular there is a problem with Boolean values - if they
    haven't been specified at all then they default to an empty string - and
    this causes problems when trying to unmarshal to the Asset type which
    must specify the types of its members. The AssetSummary is just an Asset
    with any problematic fields removed, so the json unmarshaller will
    ignore them.

--- client/assets.go
+++ client/assets.go
@@ -3,21 +3,29 @@ package client
 import "time"
 
 type Asset struct {
-	Id           string    `json:"asset_id"`
-	Name         string    `json:"name"`
-	Uri          string    `json:"uri"`
-	Start        time.Time `json:"start_date"`
-	End          time.Time `json:"end_date"`
-	Duration     int64     `json:"duration"`
-	MimeType     string    `json:"mimetype"`
-	IsActive     bool      `json:"is_active,omitempty"`
-	IsEnabled    string    `json:"is_enabled,omitempty"`
-	IsProcessing string    `json:"is_processing,omitempty"`
-	NoCache      string    `json:"nocache,omitempty"`
+	Id        string    `json:"asset_id"`
+	Name      string    `json:"name"`
+	Uri       string    `json:"uri"`
+	Start     time.Time `json:"start_date"`
+	End       time.Time `json:"end_date"`
+	Duration  int64     `json:"duration"`
+	MimeType  string    `json:"mimetype"`
+	IsActive  bool      `json:"is_active,omitempty"`
+	IsEnabled string    `json:"is_enabled,omitempty"`
+	NoCache   string    `json:"nocache,omitempty"`
 }
 
+type AssetSummary struct {
+	Id       string    `json:"asset_id"`
+	Name     string    `json:"name"`
+	Uri      string    `json:"uri"`
+	Start    time.Time `json:"start_date"`
+	End      time.Time `json:"end_date"`
+	MimeType string    `json:"mimetype"`
+	IsActive bool      `json:"is_active,omitempty"`
+}
 type PlayList struct {
-	Assets []Asset
+	Assets []AssetSummary
 }
 
 func (p *PlayList) IsEmpty() bool {
